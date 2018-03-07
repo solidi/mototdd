f533a49ac112243517de39be32baf78571afb0da
Douglas Arcuri <douglas.arcuri@viacom.com>
2018-03-06 20:48:09 -0500

    Create basic Android project template
    Include .idea code style

---
+++ katana/src/test/java/com/mototdd/katana/MainTest.kt
@@ -0,0 +1,11 @@
+package com.mototdd.katana
+
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+class MainTest {
+    @Test
+    fun `Given nothing, then I expect a running test rig`() {
+        assertTrue(true)
+    }
+}
